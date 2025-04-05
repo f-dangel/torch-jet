e276b9070786388b14d841bcaa908a63d0d2f836
Felix Dangel <felix.dangel@vectorinstitute.ai>
2025-04-05 16:00:03 -0400

    [ADD] Bilaplacian implementations with functorch and Taylor mode

---
+++ jet/bilaplacian.py
@@ -0,0 +1,110 @@
+"""Implements computing the Bi-Laplacian operator with Taylor mode."""
+
+from typing import Callable, Tuple
+
+from torch import Tensor, eye, zeros
+from torch.nn import Module
+
+from jet import jet
+from jet.utils import replicate, sum_vmapped
+
+
+class Bilaplacian(Module):
+    """Module that computes the Bi-Laplacian of a function using jets.
+
+    The Bi-Laplacian of a function f(x) ∈ R with x ∈ Rⁿ
+    is defined as the Laplacian of the Laplacian, or
+    Δf(x) = ∑ᵢ ∑ⱼ ∂⁴f(x) / ∂xᵢ²∂xⱼ² ∈ R.
+    For functions that produce vectors or tensors, the Bi-Laplacian
+    is defined per output component and has the same shape as f(x).
+    """
+
+    def __init__(
+        self, f: Callable[[Tensor], Tensor], dummy_x: Tensor, is_batched: bool
+    ):
+        """Initialize the Bi-Laplacian module.
+
+        Args:
+            f: The function whose Bi-Laplacian is computed.
+            dummy_x: The input on which the Bi-Laplacian is computed. It is only used to
+                infer meta-data of the function input that `torch.fx` is not capable
+                of determining at the moment.
+            is_batched: Whether the function and its input are batched. In this case,
+                we can use that computations can be carried out independently along
+                the leading dimension of tensors.
+        """
+        super().__init__()
+        self.jet_f = jet(f, 6, vmap=True)
+        # data that needs to be inferred explicitly from a dummy input
+        # because `torch.fx` cannot do this.
+        self.x_shape = dummy_x.shape
+        self.x_kwargs = {"dtype": dummy_x.dtype, "device": dummy_x.device}
+
+        self.unbatched_dim = (self.x_shape[1:] if is_batched else self.x_shape).numel()
+        self.batched_dim = self.x_shape[0] if is_batched else 1
+        self.is_batched = is_batched
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Compute the Bi-Laplacian of the function at the input tensor.
+
+        Args:
+            x: Input tensor. Must have same shape as the dummy input tensor that was
+                passed in the constructor.
+
+        Returns:
+            The Bi-Laplacian. Has the same shape as f(x).
+        """
+        X0, X1, X2, X3, X4, X5, X6 = self.set_up_taylor_coefficients(x)
+        _, _, _, _, _, _, F6_1 = self.jet_f(X0, X1, X2, X3, X4, X5, X6)
+        _, _, _, _, _, _, F6_2 = self.jet_f(X0, X1, -1 * X2, X3, X4, X5, X6)
+        _, _, _, _, _, _, F6_3 = self.jet_f(X0, X1, 0 * X2, X3, X4, X5, X6)
+
+        return sum_vmapped((F6_1 + F6_2 - 2 * F6_3) / 90)
+
+    def set_up_taylor_coefficients(
+        self, x: Tensor
+    ) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+        """Create the Taylor coefficients for the Bi-Laplacian computation.
+
+        Args:
+            x: Input tensor. Must have same shape as the dummy input tensor that was
+                passed in the constructor.
+
+        Returns:
+            The six input tensors to the 6-jet that computes the Bi-Laplacian.
+        """
+        X0 = replicate(x, self.unbatched_dim**2)
+        X3 = zeros(self.unbatched_dim**2, *self.x_shape, **self.x_kwargs)
+        X4 = zeros(self.unbatched_dim**2, *self.x_shape, **self.x_kwargs)
+        X5 = zeros(self.unbatched_dim**2, *self.x_shape, **self.x_kwargs)
+        X6 = zeros(self.unbatched_dim**2, *self.x_shape, **self.x_kwargs)
+
+        X1 = (
+            eye(self.unbatched_dim, **self.x_kwargs)
+            .unsqueeze(0)
+            .repeat(self.unbatched_dim, 1, 1)
+        )
+        if self.is_batched:
+            X1 = X1.reshape(self.unbatched_dim**2, 1, *self.x_shape[1:])
+            # copy without using more memory
+            X1 = X1.expand(-1, self.batched_dim, *(-1 for _ in self.x_shape[1:]))
+        else:
+            X1 = X1.reshape(self.unbatched_dim**2, *self.x_shape)
+
+        # In comparison to X1, note how for X2 we have to change the order of the
+        # canonical basis vectors so that we loop over all possible combinations (i, j).
+        # This is done by repeating the identity matrix along the middle, instead of the
+        # leading, axis.
+        X2 = (
+            eye(self.unbatched_dim, **self.x_kwargs)
+            .unsqueeze(1)
+            .repeat(1, self.unbatched_dim, 1)
+        )
+        if self.is_batched:
+            X2 = X2.reshape(self.unbatched_dim**2, 1, *self.x_shape[1:])
+            # copy without using more memory
+            X2 = X2.expand(-1, self.batched_dim, *(-1 for _ in self.x_shape[1:]))
+        else:
+            X2 = X2.reshape(self.unbatched_dim**2, *self.x_shape)
+
+        return X0, X1, X2, X3, X4, X5, X6
---
+++ test/test_bilaplacian.py
@@ -0,0 +1,128 @@
+"""Tests the computation of Bi-Laplacians.
+
+The Bi-Laplacian of a function f(x) ∈ R with x ∈ Rⁿ is defined as the Laplacian of the
+Laplacian, or Δf(x) = ∑ᵢ ∑ⱼ ∂⁴f(x) / ∂xᵢ²∂xⱼ² ∈ R where the sum ranges to n.
+
+For functions that produce vectors or tensors, the Bi-Laplacian is defined per output
+component. It has the same shape as f(x).
+"""
+
+from test.test___init__ import (
+    CASES_COMPACT,
+    CASES_COMPACT_IDS,
+    report_nonclose,
+    setup_case,
+)
+from typing import Any, Callable, Dict
+
+from einops import einsum
+from pytest import mark
+from torch import Tensor, zeros, zeros_like
+from torch.autograd import grad
+from torch.func import hessian, vmap
+
+from jet.bilaplacian import Bilaplacian
+
+
+def bilaplacian(f: Callable[[Tensor], Tensor], X: Tensor, is_batched: bool) -> Tensor:
+    """Compute the Bi-Laplacian by taking the trace of the fourth derivative tensor.
+
+    Args:
+        f: The function to compute the Bi-Laplacian of.
+        X: The point at which to compute the Bi-Laplacian.
+        is_batched: Whether the input tensor X is batched.
+
+    Returns:
+        The Bi-Laplacian of the function f at the point X, evaluated
+        for each element f[i](X). Has same shape as f(X).
+    """
+    # compute the derivative tensor of fourth derivatives
+    # TODO Check if changing the inner Hessian to rev-rev improves performance
+    # TODO Look into making this more efficient by directly contracting with the
+    # correct unit vectors, similar to the vHv-approach for the randomized Laplacian.
+    d4f = hessian(hessian(f))
+
+    if is_batched:
+        d4f = vmap(d4f)
+    d4f_X = d4f(X)
+
+    # trace it using einsum to support functions with non-scalar outputs
+    num_summed_dims = X.ndim - 1 if is_batched else X.ndim
+    dims1 = " ".join([f"i{i}" for i in range(num_summed_dims)])
+    dims2 = " ".join([f"j{j}" for j in range(num_summed_dims)])
+    batch_str = "n " if is_batched else ""
+    # if x is a vector, this is just '... i i j j -> ...' where '...' corresponds
+    # to the shape of f(x)
+    equation = f"{batch_str}... {dims1} {dims1} {dims2} {dims2} -> {batch_str}..."
+
+    return einsum(d4f_X, equation)
+
+
+def bilaplacian_naive(
+    f: Callable[[Tensor], Tensor], X: Tensor, detach: bool = True
+) -> Tensor:
+    """Naive implementation of the Bi-Laplacian via a for-loop.
+
+    Args:
+        f: The function to compute the Bi-Laplacian of.
+        X: The point at which to compute the Bi-Laplacian. Must have `requires_grad`.
+        detach: Whether to detach the result from the compute graph. Default: `True`.
+
+    Returns:
+        The Bi-Laplacian of the function f at the point X, evaluated
+        for each element f[i](X). Has same shape as f(X).
+    """
+    D = X.numel()
+    f_X = f(X)
+    bilap = zeros_like(f_X).flatten()
+
+    # loop over all components of f(X) and compute their bi-Laplacian
+    for k, f_k in enumerate(f_X.flatten()):
+
+        # take the second derivative w.r.t. xᵢ
+        for i in range(D):
+            e_i = zeros(D, dtype=X.dtype, device=X.device)
+            e_i[i] = 1
+            e_i = e_i.view_as(X)
+
+            (df,) = grad(f_k, X, create_graph=True)
+            (d2f,) = grad((df * e_i).sum(), X, create_graph=True)
+
+            # the third derivative can be re-cycled for all xⱼ
+            d2f_ii = (d2f * e_i).sum()
+            (d3f,) = grad(d2f_ii, X, create_graph=True)
+
+            # differentiate twice w.r.t. xⱼ
+            for j in range(D):
+                e_j = zeros(D, dtype=X.dtype, device=X.device)
+                e_j[j] = 1
+                e_j = e_j.view_as(X)
+
+                (d4f,) = grad((d3f * e_j).sum(), X, create_graph=True)
+                d4f_ii_jj = (d4f * e_j).sum()
+
+                bilap[k] += d4f_ii_jj.detach() if detach else d4f_ii_jj
+
+    return bilap.reshape_as(f_X)
+
+
+@mark.parametrize("config", CASES_COMPACT, ids=CASES_COMPACT_IDS)
+def test_bilaplacian(config: Dict[str, Any]):
+    """Compare Laplacian implementations.
+
+    Args:
+        config: Configuration dictionary of the test case.
+    """
+    f, x, _, is_batched = setup_case(config, taylor_coefficients=False)
+
+    # reference: Using PyTorch's autograd
+    bilap_rev = bilaplacian_naive(f, x.clone().requires_grad_())
+
+    # using torch.func
+    bilap_func = bilaplacian(f, x, is_batched)
+    report_nonclose(bilap_rev, bilap_func, name="Naive and functorch Bi-Laplacians")
+
+    # using jets
+    bilap_mod = Bilaplacian(f, x, is_batched)
+    bilap_jet = bilap_mod(x)
+    report_nonclose(bilap_rev, bilap_jet, name="Naive and jet Bi-Laplacians")

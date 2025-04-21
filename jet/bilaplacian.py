85ff0cb6d70281740f435bb1392004a1de1798da
Felix Dangel <felix.dangel@vectorinstitute.ai>
2025-04-21 14:48:49 -0400

    [REF] Implement exact Bi-Laplacian with 4-jets

--- jet/bilaplacian.py
+++ jet/bilaplacian.py
@@ -40,7 +40,7 @@ class Bilaplacian(Module):
                 the leading dimension of tensors.
         """
         super().__init__()
-        self.jet_f = jet(f, 6, vmap=True)
+        self.jet_f = jet(f, 4, vmap=True)
         # data that needs to be inferred explicitly from a dummy input
         # because `torch.fx` cannot do this.
         self.x_shape = dummy_x.shape
@@ -60,16 +60,42 @@ class Bilaplacian(Module):
         Returns:
             The Bi-Laplacian. Has the same shape as f(x).
         """
-        X0, X1, X2, X3, X4, X5, X6 = self.set_up_taylor_coefficients(x)
-        _, _, _, _, _, _, F6_1 = self.jet_f(X0, X1, X2, X3, X4, X5, X6)
-        _, _, _, _, _, _, F6_2 = self.jet_f(X0, X1, -1 * X2, X3, X4, X5, X6)
-        _, _, _, _, _, _, F6_3 = self.jet_f(X0, X1, 0 * X2, X3, X4, X5, X6)
+        # three lists of 4-jet coefficients, one for each term
+        C1, C2, C3 = self.set_up_taylor_coefficients(x)
 
-        return sum_vmapped((F6_1 + F6_2 - 2 * F6_3) / 90)
+        gamma_4_4 = 3 / 32
+        gamma_4_0 = 13 / 192
+        # first summand
+        _, _, _, _, F4_1 = self.jet_f(*C1)
+        # NOTE Needs a 1/24 here to match the current write-up in the paper
+        factor1 = (gamma_4_4 + 2 * (self.unbatched_dim - 1) * gamma_4_0) / 24
+        term1 = factor1 * sum_vmapped(F4_1)
 
-    def set_up_taylor_coefficients(
-        self, x: Tensor
-    ) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+        # there are no off-diagonal terms if the dimension is 1
+        if self.unbatched_dim == 1:
+            return term1
+
+        # second summand
+        gamma_3_1 = -1 / 3
+        _, _, _, _, F4_2 = self.jet_f(*C2)
+        # NOTE Needs a 1/24 here to match the current write-up in the paper
+        factor2 = 2 * gamma_3_1 / 24
+        term2 = factor2 * sum_vmapped(F4_2)
+
+        # third term
+        gamma_2_2 = 5 / 8
+        _, _, _, _, F4_3 = self.jet_f(*C3)
+        # NOTE Needs a 1/24 here to match the current write-up in the paper
+        factor3 = gamma_2_2 / 24
+        term3 = factor3 * sum_vmapped(F4_3)
+
+        return term1 + term2 + term3
+
+    def set_up_taylor_coefficients(self, x: Tensor) -> Tuple[
+        Tuple[Tensor, Tensor, Tensor, Tensor, Tensor],
+        Tuple[Tensor, Tensor, Tensor, Tensor, Tensor],
+        Tuple[Tensor, Tensor, Tensor, Tensor, Tensor],
+    ]:
         """Create the Taylor coefficients for the Bi-Laplacian computation.
 
         Args:
@@ -77,43 +103,71 @@ class Bilaplacian(Module):
                 passed in the constructor.
 
         Returns:
-            The six input tensors to the 6-jet that computes the Bi-Laplacian.
+            A tuple containing the inputs to the three 4-jets.
         """
-        X0 = replicate(x, self.unbatched_dim**2)
-        X3 = zeros(self.unbatched_dim**2, *self.x_shape, **self.x_kwargs)
-        X4 = zeros(self.unbatched_dim**2, *self.x_shape, **self.x_kwargs)
-        X5 = zeros(self.unbatched_dim**2, *self.x_shape, **self.x_kwargs)
-        X6 = zeros(self.unbatched_dim**2, *self.x_shape, **self.x_kwargs)
+        D = self.unbatched_dim
 
-        X1 = (
-            eye(self.unbatched_dim, **self.x_kwargs)
-            .unsqueeze(0)
-            .repeat(self.unbatched_dim, 1, 1)
-        )
+        # first 4-jet
+        X1_0 = replicate(x, D)
+        X1_2 = zeros(D, *self.x_shape, **self.x_kwargs)
+        X1_3 = zeros(D, *self.x_shape, **self.x_kwargs)
+        X1_4 = zeros(D, *self.x_shape, **self.x_kwargs)
+
+        X1_1 = 4 * eye(D, **self.x_kwargs)
         if self.is_batched:
-            X1 = X1.reshape(self.unbatched_dim**2, 1, *self.x_shape[1:])
+            X1_1 = X1_1.reshape(D, 1, *self.x_shape[1:])
             # copy without using more memory
-            X1 = X1.expand(-1, self.batched_dim, *(-1 for _ in self.x_shape[1:]))
+            X1_1 = X1_1.expand(-1, self.batched_dim, *(-1 for _ in self.x_shape[1:]))
         else:
-            X1 = X1.reshape(self.unbatched_dim**2, *self.x_shape)
+            X1_1 = X1_1.reshape(D, *self.x_shape)
 
-        # In comparison to X1, note how for X2 we have to change the order of the
-        # canonical basis vectors so that we loop over all possible combinations (i, j).
-        # This is done by repeating the identity matrix along the middle, instead of the
-        # leading, axis.
-        X2 = (
-            eye(self.unbatched_dim, **self.x_kwargs)
-            .unsqueeze(1)
-            .repeat(1, self.unbatched_dim, 1)
-        )
+        C1 = (X1_0, X1_1, X1_2, X1_3, X1_4)
+
+        # second 4-jet
+        X2_0 = replicate(x, D * (D - 1))
+        X2_2 = zeros(D * (D - 1), *self.x_shape, **self.x_kwargs)
+        X2_3 = zeros(D * (D - 1), *self.x_shape, **self.x_kwargs)
+        X2_4 = zeros(D * (D - 1), *self.x_shape, **self.x_kwargs)
+
+        X2_1 = zeros(D, D - 1, D, **self.x_kwargs)
+        for i in range(D):
+            not_i = [j for j in range(D) if i != j]
+            for j_idx, j in enumerate(not_i):
+                X2_1[i, j_idx, i] = 3
+                X2_1[i, j_idx, j] = 1
+
         if self.is_batched:
-            X2 = X2.reshape(self.unbatched_dim**2, 1, *self.x_shape[1:])
+            X2_1 = X2_1.reshape(D * (D - 1), 1, *self.x_shape[1:])
             # copy without using more memory
-            X2 = X2.expand(-1, self.batched_dim, *(-1 for _ in self.x_shape[1:]))
+            X2_1 = X2_1.expand(-1, self.batched_dim, *(-1 for _ in self.x_shape[1:]))
         else:
-            X2 = X2.reshape(self.unbatched_dim**2, *self.x_shape)
+            X2_1 = X2_1.reshape(D * (D - 1), *self.x_shape)
 
-        return X0, X1, X2, X3, X4, X5, X6
+        C2 = (X2_0, X2_1, X2_2, X2_3, X2_4)
+
+        # third 4-jet
+        X3_0 = replicate(x, D * (D - 1))
+        X3_2 = zeros(D * (D - 1), *self.x_shape, **self.x_kwargs)
+        X3_3 = zeros(D * (D - 1), *self.x_shape, **self.x_kwargs)
+        X3_4 = zeros(D * (D - 1), *self.x_shape, **self.x_kwargs)
+
+        X3_1 = zeros(D, D - 1, D, **self.x_kwargs)
+        for i in range(D):
+            not_i = [j for j in range(D) if i != j]
+            for j_idx, j in enumerate(not_i):
+                X3_1[i, j_idx, i] = 2
+                X3_1[i, j_idx, j] = 2
+
+        if self.is_batched:
+            X3_1 = X3_1.reshape(D * (D - 1), 1, *self.x_shape[1:])
+            # copy without using more memory
+            X3_1 = X3_1.expand(-1, self.batched_dim, *(-1 for _ in self.x_shape[1:]))
+        else:
+            X3_1 = X3_1.reshape(D * (D - 1), *self.x_shape)
+
+        C3 = (X3_0, X3_1, X3_2, X3_3, X3_4)
+
+        return C1, C2, C3
 
 
 class RandomizedBilaplacian(Bilaplacian):
@@ -165,9 +219,6 @@ class RandomizedBilaplacian(Bilaplacian):
         self.distribution = distribution
         self.sample_func = {"normal": randn}[distribution]
         self.num_samples = num_samples
-        # TODO Remove this once the exact Laplacian is implemented with 4-jets
-        # rather than 6-jets
-        self.jet_f = jet(f, 4, vmap=True)
 
     def forward(self, x: Tensor) -> Tensor:
         """Compute the MC-Bi-Laplacian of the function at the input tensor.

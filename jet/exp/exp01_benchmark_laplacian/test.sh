aa67088119c3414b4a0dc31a05683143c352e6a0
Felix Dangel <felix.dangel@vectorinstitute.ai>
2025-04-30 14:16:59 +0800

    [DEL] Remove test script [no ci]

--- jet/exp/exp01_benchmark_laplacian/test.sh
+++
@@ -1,3 +0,0 @@
-python execute.py --architecture=tanh_mlp_768_768_512_512_1 --dim=10 --batch_size=32 --strategy=hessian_trace --device=cpu --operator=laplacian --use_jax
-python execute.py --architecture=tanh_mlp_768_768_512_512_1 --dim=10 --batch_size=32 --strategy=jet_naive --device=cpu --operator=laplacian --use_jax
-python execute.py --architecture=tanh_mlp_768_768_512_512_1 --dim=10 --batch_size=32 --strategy=jet_simplified --device=cpu --operator=laplacian --use_jax
